256b9b78873b55410afa205c87bdf7449c0f212d
Yell Min Thu <120482584+JunHaoyell@users.noreply.github.com>
2026-03-10 01:56:13 +0700

    Update rag_pipeline.py

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -1,34 +1,23 @@
-from langchain_community.vectorstores import FAISS
-from langchain_huggingface import HuggingFaceEmbeddings
-from groq import Groq
 import os
 from dotenv import load_dotenv
+from langchain.vectorstores import FAISS
+from langchain.embeddings import HuggingFaceEmbeddings
+from groq import Groq
 
 load_dotenv()
 
-embedding = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+VECTOR_PATH = os.path.join(BASE_DIR, "vector_store")
 
-db = FAISS.load_local(
-    "../vector_store",
-    embedding,
-    allow_dangerous_deserialization=True
-)
+embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+db = FAISS.load_local(VECTOR_PATH, embedding, allow_dangerous_deserialization=True)
 
-client = Groq(
-    api_key=os.getenv("GROQ_API_KEY")
-)
-
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 def ask_rag(question):
-
     docs = db.similarity_search(question, k=3)
-
-    context = "\n\n".join(
-        [f"文档{i+1}:\n{doc.page_content}" for i, doc in enumerate(docs)]
-    )
-
+    context = "\n\n".join([f"文档{i+1}:\n{doc.page_content}" for i, doc in enumerate(docs)])
+    
     prompt = f"""
 你是一个企业知识库AI助手。
 
@@ -45,10 +34,7 @@ def ask_rag(question):
 """
 
     response = client.chat.completions.create(
-        messages=[
-            {"role": "user", "content": prompt}
-        ],
+        messages=[{"role": "user", "content": prompt}],
         model="llama-3.1-8b-instant"
     )
-
     return response.choices[0].message.content
